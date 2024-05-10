30b54135382552afc79eb8f9f14a72aea21435c4
Lina Persson Signell <linasignell@live.se>
2024-05-10 17:56:07 +0200

    added style form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,11 +1,19 @@
 import ContactForm from '../components/form';
 import Header from '../components/header';
 import Footer from '../components/footer';
+import '../styles/contact.css';
 
 const Contact = () => {
   return (
     <div className="page-container">
       <Header />
+      <p className="contactinfo">
+        Jag nås lättast på mail: linasignell@live.se <br /> Men det går även bra
+        att ringa mig på telefonnummer: +46707333097
+        <br />
+        Det går även bra att skriva till mig via kontaktformuläret nedan så hör
+        jag av mig så fort jag kan!
+      </p>
       <ContactForm />
       <Footer />
     </div>
