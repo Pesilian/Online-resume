1d8a95ec3c22d88a02965d63c1399d215c80bb6f
Lina Persson Signell <linasignell@live.se>
2024-05-10 18:45:43 +0200

    added style

--- src/components/GitAPIaxios.jsx
+++ src/components/GitAPIaxios.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/api.css';
 
 function GitAPI(props) {
   const [repos, setRepos] = useState([]);
@@ -16,12 +17,12 @@ function GitAPI(props) {
   }, [props.username]); // Dependency array med username så att förfrågan körs om när username ändras
 
   return (
-    <div>
-      <h1>Github för {props.username}</h1>
+    <div className="api-container">
+      <h2 className="heading">Mina Projekt</h2>
 
-      <ul>
+      <ul className="list">
         {repos.map(repo => (
-          <li key={repo.id}>
+          <li className="listitem" key={repo.id}>
             <a href={repo.svn_url}>{repo.name}</a>
           </li>
         ))}
--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,11 +1,27 @@
 import Header from '../components/header';
 import GitAPI from '../components/GitAPIaxios';
 import Footer from '../components/footer';
+import { useNavigate } from 'react-router-dom';
+
+import '../styles/projects.css';
 
 const Projects = () => {
+  const nav = useNavigate();
+
+  const handleClick = () => {
+    nav('/kontakt');
+  };
   return (
     <div className="page-container">
       <Header />
+      <p className="apiinfo">
+        Här kan du se mina repos på github.
+        <br /> Har du någon feedback eller kommentar får du gärna skicka ett
+        meddelande via mitt formulär som du hittar{' '}
+        <a className="formlink" onClick={handleClick}>
+          här!
+        </a>
+      </p>
       <GitAPI username="pesilian" />
       <Footer />
     </div>
